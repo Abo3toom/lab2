966814758109dfd1b8f26bea93c9de5cb5282c67
Abo3toom <82682336+Abo3toom@users.noreply.github.com>
2021-05-22 18:20:24 +0300

    Update mcf.cpp

--- app/mcf.cpp
+++ app/mcf.cpp
@@ -1,10 +1,18 @@
 #include "_text.h"
-#include <string.h>
 
-void mcf(text txt) 
+
+void mcf(text txt)
 {
-    
-    txt->cursor->position++;
+     auto current = txt->lines->begin();
+
+     if (txt->lines->size() != 0)
+        {
+        txt->lines->remove(*current) ;
+    }
+     else if (txt == NULL || txt->lines->size() == 0)
+        {
+        fprintf(stderr, "There are already no any lines in the text!\n");
+            return;
+        }
 
 }
-
