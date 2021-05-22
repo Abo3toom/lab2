669a9c67c3cd7df85509eca19dd35332feff8169
Abo3toom <82682336+Abo3toom@users.noreply.github.com>
2021-05-22 19:13:43 +0300

    Update shownumspaces.cpp

--- app/shownumspaces.cpp
+++ app/shownumspaces.cpp
@@ -13,16 +13,14 @@ void shownumspaces(text txt)
 
 static void show_line(int index, char *contents, int cursor, void *data)
 {
-    /* Функция обработчик всегда получает существующую строку */
-    assert(contents != NULL);
-
     /* Декларируем неиспользуемые параметры */
     UNUSED(index);
     UNUSED(cursor);
     UNUSED(data);
     // calculate the spaces 
+
     int Spaces = 0;
-    for(int i = 0 ; contents[i] != '\0' ; i++)
+    for(int i = 0 ; (int)sizeof(contents) != '\0' ; i++)
     {
 	if (contents[i] == ' ')
       	{
