6c411e8623b54df5934761343f8e8c64e281f814
Abo3toom <82682336+Abo3toom@users.noreply.github.com>
2021-05-22 18:43:04 +0300

    Update mcf.h

--- tests/mcf.h
+++ tests/mcf.h
@@ -4,3 +4,51 @@
 #include "common.h"
 #include "text.h"
 #include "_text.h"
+
+TEST(mcf, normal)
+{
+    text txt = create_text();
+    txt->lines->push_back("line 0\n");
+    txt->lines->push_back("line 1\n");
+    txt->lines->push_back("line 2\n");
+    txt->cursor->line_num = 0;
+    txt->cursor->position = 0;
+
+    mcf(txt);
+
+    EXPECT_EQ(txt->cursor->line_num, 1);
+    EXPECT_EQ(txt->cursor->position, 0);
+
+    free(txt);
+}
+
+TEST(mcf, last_line)
+{
+    text txt = create_text();
+    txt->lines->push_back("line 0\n");
+    txt->lines->push_back("line 1\n");
+    txt->lines->push_back("line 2\n");
+    txt->cursor->line_num = 2;
+    txt->cursor->position = 0;
+
+    mcf(txt);
+
+    EXPECT_EQ(txt->cursor->line_num, 2);
+    EXPECT_EQ(txt->cursor->position, 0);
+
+    free(txt);
+}
+
+TEST(mcf, empty_text)
+{
+    text txt = create_text();
+    txt->cursor->line_num = -1; // при создании пустого текста
+    txt->cursor->position = -1; // значения -1 -1
+
+    mcf(txt);
+
+    EXPECT_EQ(txt->cursor->line_num, 0);
+    EXPECT_EQ(txt->cursor->position, 0);
+
+    free(txt);
+}
