83c6813f0c41d1467cd00536a1a03d39f6b236c2
Abo3toom <82682336+Abo3toom@users.noreply.github.com>
2021-05-22 21:01:04 +0300

    Add files via upload

---
+++ tests/rh.h
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "common.h"
+#include "text.h"
+#include "_text.h"
+
+TEST(rh , first_line)
+{
+    text txt = create_text();
+    txt->lines->push_back("line 0\n");
+    txt->lines->push_back("line 1\n");
+    txt->lines->push_back("line 2\n");
+
+    rh(txt);
+    EXPECT_EQ(txt->lines->size(), 2);
+}
+
+TEST(rh , no_lines)
+{
+    text txt = create_text();
+    txt->cursor->line_num = -1;
+    txt->cursor->position = -1;
+
+    rh(txt);
+    EXPECT_EQ(txt->lines->size(), 0);
+}
