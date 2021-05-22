d5b2e02ff415549b101b769324c0ded902f41a16
Abo3toom <82682336+Abo3toom@users.noreply.github.com>
2021-05-23 00:38:26 +0300

    Update and rename shownumspaces.h to mcf.h

---
+++ tests/mcf.h
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "common.h"
+#include "text.h"
+#include "_text.h"
+
+TEST(mcf, normal)
+{
+    text txt = create_text();
+    txt->lines->push_back("line 0\n");
+    txt->lines->push_back("line 1\n");
+    txt->lines->push_back("line 2\n");
+    txt->cursor->line_num = 0;
+    txt->cursor->position = 0;
+
+
+    mcf(txt);
+    EXPECT_EQ(txt->cursor->line_num, 0);
+    EXPECT_EQ(txt->cursor->position, 1);
+
+
+    free(txt);
+}
+
+TEST(mcf, secondline)
+{
+    text txt = create_text();
+    txt->lines->push_back("line 0\n");
+    txt->lines->push_back("line 1\n");
+    txt->lines->push_back("line 2\n");
+    txt->cursor->line_num = 0;
+    txt->cursor->position = 0;
+
+    mcf(txt, 1, 3);
+    shownumspaces(txt);
+    EXPECT_EQ(txt->cursor->line_num, 1);
+    EXPECT_EQ(txt->cursor->position, 4);
+
+    free(txt);
+}
+
+
--- tests/shownumspaces.h
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock-matchers.h>
-
-#include "common.h"
-#include "text.h"
-#include "_text.h"
-
